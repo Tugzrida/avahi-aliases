a670e971b611d19e831668295a2ba2eb499218f5
Tugzrida <git@tugzrida.xyz>
2020-08-21 19:38:01 +1000

    Finish migration to python3 and update install script.

--- install.sh
+++ install.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
 printf "\e[92mInstalling avahi-aliases...\e[39m\n"
 
 printf "\e[92mInstalling dependencies...\e[39m\n"
-sudo apt-get -y update
-sudo apt-get -y install avahi-daemon python-avahi git
-
-printf "\e[92mCloning avahi-aliases...\e[39m\n"
-git clone https://github.com/Tugzrida/avahi-aliases.git /tmp/avahi-aliases/
+apt-get -y update
+apt-get -y install avahi-daemon python3-dbus
 
 printf "\e[92mCopying files...\e[39m\n"
-sudo cp /tmp/avahi-aliases/avahi-publish-aliases /tmp/avahi-aliases/avahi-list-aliases /tmp/avahi-aliases/avahi-add-aliases /tmp/avahi-aliases/avahi-remove-aliases /usr/bin/
-sudo cp /tmp/avahi-aliases/avahi-aliases.service /etc/systemd/system/
-sudo mkdir /etc/avahi/aliases.d/
-sudo touch /etc/avahi/aliases
-rm -rf /tmp/avahi-aliases/
+script_dir=$(dirname "$0")
+cp "$script_dir/avahi-publish-aliases" "$script_dir/avahi-list-aliases" "$script_dir/avahi-add-aliases" "$script_dir/avahi-remove-aliases" /usr/bin/
+cp "$script_dir/avahi-aliases.service" /etc/systemd/system/
+mkdir /etc/avahi/aliases.d/
+touch /etc/avahi/aliases
 
 printf "\e[92mSetting permissions...\e[39m\n"
-sudo chown root:root /usr/bin/avahi-publish-aliases /usr/bin/avahi-list-aliases /usr/bin/avahi-add-aliases /usr/bin/avahi-remove-aliases /etc/systemd/system/avahi-aliases.service
-sudo chmod 755 /usr/bin/avahi-publish-aliases /usr/bin/avahi-list-aliases /usr/bin/avahi-add-aliases /usr/bin/avahi-remove-aliases
-sudo chmod 644 /etc/systemd/system/avahi-aliases.service
+chown root:root /usr/bin/avahi-publish-aliases /usr/bin/avahi-list-aliases /usr/bin/avahi-add-aliases /usr/bin/avahi-remove-aliases /etc/systemd/system/avahi-aliases.service
+chmod 755 /usr/bin/avahi-publish-aliases /usr/bin/avahi-list-aliases /usr/bin/avahi-add-aliases /usr/bin/avahi-remove-aliases
+chmod 644 /etc/systemd/system/avahi-aliases.service
 
 printf "\e[92mStarting up...\e[39m\n"
-sudo systemctl daemon-reload
-sudo systemctl enable avahi-aliases.service
-sudo systemctl restart avahi-daemon.service
+systemctl daemon-reload
+systemctl enable avahi-aliases.service
+systemctl restart avahi-daemon.service
 
 printf "\e[92mDone! Add/remove aliases with \e[36msudo avahi-add-aliases \"alias.local\"\e[92m and \e[36msudo avahi-remove-aliases \"alias.local\"\e[39m\n"
 printf "\e[92mList active aliases with \e[36mavahi-list-aliases\e[39m\n"
